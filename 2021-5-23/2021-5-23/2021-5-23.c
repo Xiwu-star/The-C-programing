e7c5fc3a6fc7f2ddda8c13ca0cd289c77e63338b
Xiwu-star <1668389949@qq.com>
2021-05-30 23:04:43 +0800

    提交代码

---
+++ 2021-5-23/2021-5-23/2021-5-23.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	//char ch='A' ;
+	//printf("%c\n", ch);
+	//int age = 20;
+	//printf("%d\n", age);
+	//long num = 100;
+	//printf("%d\n", num);
+	//float f = 5.0;
+	//printf("%f\n", f);
+	//double d = 3.14;
+	//printf("%lf\n", d);
+	return 0;
+}
